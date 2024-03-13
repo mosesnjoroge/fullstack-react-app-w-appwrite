511c442ddb8967c06aadd9e2e48e1d0384bffcab
mosesnjoroge <mosesbnjoroge@outlook.com>
2024-03-13 12:40:36 -0400

    created an sync function to redirect post validation

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,11 @@ import {account} from '@/lib/appwrite';
 import {ID} from 'appwrite'
 
 export async function logIn(email:string) {
-  const data =await account.createMagicURLSession(ID.unique(), email)
+  const data =await account.createMagicURLSession(ID.unique(), email, `${window.location.origin}/session`)
+  return data;
+}
+
+export async function verifySession(userId:string, secret: string) {
+  const data = await account.updateMagicURLSession(userId, secret);
   return data;
 }
--- src/pages/session.tsx
+++ src/pages/session.tsx
@@ -1,6 +1,24 @@
+import {useEffect} from 'react'
+import { verifySession } from '@/lib/auth';
 import Container from '@/components/Container';
+import { useEffect } from 'react';
+import { URLSearchParams } from 'url';
 
 function Session() {
+  useEffect (() =>{
+    const params = new URLSearchParams(window.location.search);
+    const userId = params.get('userId');
+    const secret = params.get('secret');
+
+    if(typeof userId !== 'string' || typeof secret !== 'string'){
+      return;
+    }
+
+    (async function run() {
+      await verifySession(userId,secret)
+    })();
+  },[]);
+
   return (
     <Container className="h-screen flex items-center justify-center text-center">
       <p>Logging you in...</p>
