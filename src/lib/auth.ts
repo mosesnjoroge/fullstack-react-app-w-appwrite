f89ddf3b99543eb9e97f705859e9b606a04bc130
mosesnjoroge <mosesbnjoroge@outlook.com>
2024-03-13 12:22:18 -0400

    set up log in function with magic link

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import {account} from '@/lib/appwrite';
+import {ID} from 'appwrite'
 
-
-export async function logIn() {
-  await account.createMagicURLSession(uniqueId, email)
+export async function logIn(email:string) {
+  const data =await account.createMagicURLSession(ID.unique(), email)
+  return data;
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,5 @@
+import { logIn } from '@/lib/auth';
+
 import Layout from '@/components/Layout';
 import Container from '@/components/Container';
 import FormRow from '@/components/FormRow';
@@ -6,13 +8,22 @@ import InputText from '@/components/InputText';
 import Button from '@/components/Button';
 
 function LogIn() {
+  async function handleOnSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      email: {value:string}
+
+    }
+    await logIn(target.email.value);
+  }
   return (
     <Layout>
       <Container>
         <h1 className="text-3xl font-bold text-center mb-6">
           Log In
         </h1>
-        <form className="max-w-xs border border-slate-200 dark:border-slate-500 rounded p-6 mx-auto">
+        <form className="max-w-xs border border-slate-200 dark:border-slate-500 rounded p-6 mx-auto" onSubmit = {handleOnSubmit}>
           <FormRow className="mb-5">
             <FormLabel htmlFor="email">Email</FormLabel>
             <InputText id="email" name="email" type="email" />
